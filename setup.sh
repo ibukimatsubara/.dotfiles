fa3bd34c866657316a8fa2e4566a0248d6ab6b01
1vket <65165408+1vket@users.noreply.github.com>
2024-02-03 15:54:01 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 touch ~/.zshrc
-echo "if [ -f ~/dotfiles/zshrc ]; then" >> ~/.zshrc
-echo "   . ~/dotfiles/zshrc" >> ~/.zshrc
+echo "if [ -f ~/.dotfiles/zshrc ]; then" >> ~/.zshrc
+echo "   . ~/.dotfiles/zshrc" >> ~/.zshrc
 echo "fi" >> ~/.zshrc
 
 # link
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/nvim/dein
 mkdir -p ~/.config/nvim/toml
-ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/dein.toml ~/.config/nvim/toml/dein.toml
-ln -s ~/dotfiles/dein_lazy.toml ~/.config/nvim/toml/dein_lazy.toml
+ln -s ~/.dotfiles/init.vim ~/.config/nvim/init.vim
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/dein.toml ~/.config/nvim/toml/dein.toml
+ln -s ~/.dotfiles/dein_lazy.toml ~/.config/nvim/toml/dein_lazy.toml
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
